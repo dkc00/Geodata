9c9f1142a8f8ac91c59127cbfec8f372d01bde5d
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2025-09-10 19:24:58 +0200

    Create kerngeruest_layouts_laden.py
    Hier wird ein vorhandenes Layout (z.B. Vorlage für GIS-Projekte) eingeladen und mit dem User per Messagebox interagiert. Kann ausgebaut und in andere Skripte integriert werden.

---
+++ kerngeruest_layouts_laden.py
@@ -0,0 +1,30 @@
+from qgis.core import QgsProject, Qgis, QgsPrintLayout, QgsReadWriteContext
+from qgis.PyQt.QtXml import QDomDocument
+import os
+
+dateiPfad0 = r"..." # hier den dateipfad auswählen (qpt-Datei als Layout!)
+
+# layout_name wird aus dem dateipfad entnommen mit os 
+layout_name = os.path.splitext(os.path.basename(dateiPfad0))[0]
+
+
+project = QgsProject.instance()
+
+layout = QgsPrintLayout(project) # QgsPrintLayout von qgis.core wird benutzt
+layout.initializeDefaults()
+
+with open(dateiPfad0) as f:
+    template_content = f.read()
+    doc = QDomDocument()
+    doc.setContent(template_content)
+    items, ok = layout.loadFromTemplate(doc, QgsReadWriteContext(), True)
+    layout.setName(layout_name) # siehe variable am anfang
+    laden = project.layoutManager().addLayout(layout)
+    print(laden)
+    
+    if laden is True:
+        iface.messageBar().pushMessage('Erfolgreich geladen!', Qgis.Success, 10) 
+        # kann durch beliebige pushmessage ausgetauscht werden
+        
+    if laden is False:
+        iface.messageBar().pushMessage('Layout konnte nicht geladen werden. Ist es schon geladen?', Qgis.Warning, 10)
