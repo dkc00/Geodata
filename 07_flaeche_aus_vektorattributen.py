1063f671819ad03926a2705f38873fc2369069d9
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2025-09-12 17:08:40 +0200

    Create 07_flaeche_aus_vektorattributen.py

---
+++ 07_flaeche_aus_vektorattributen.py
@@ -0,0 +1,43 @@
+"""
+Das folgende Skript soll aus tausenden Biotop-Polygonen mit einer 
+kategorisierten Information (Spalte in der Attributtabelle) die Gesamtfläche 
+jedes Attributs ausgeben. 
+Das spart umständlichere Arbeit mit $area im Feldrechner von QGIS. 
+
+hier: einzigartige einträge aus einer Spalte namens "Hckurz".
+Das Ergebnis soll in Hektar angegeben werden, hierfür muss der m2-Wert 
+durch 10.000 geteilt werden.
+
+"""
+
+from qgis.core import QgsProject
+from qgis.PyQt.QtCore import QVariant
+
+
+lyr = QgsProject.instance().mapLayersByName('Biotope_cut')[0] 
+
+if not lyr:
+    print("layer 'Biotope_cut' nicht gefunden!")
+else:
+    print(f"Layer gefunden: {lyr.name()} mit {lyr.featureCount()} Features")
+    
+    flaechen_pro_kategorie = {} # flächen als dict speichern
+    
+    for feat in lyr.getFeatures():
+        kategorie = feat['Hckurz']
+        
+        geom = feat.geometry()
+        if geom and geom.isGeosValid():
+            flaeche = geom.area()  
+            
+            if kategorie in flaechen_pro_kategorie:
+                flaechen_pro_kategorie[kategorie] += flaeche
+            else:
+                flaechen_pro_kategorie[kategorie] = flaeche
+    
+    print("\nGesamtflächen pro Kategorie (Hckurz):")
+    print("=" * 50)
+    
+    for kategorie, gesamtflaeche in flaechen_pro_kategorie.items():
+        print(f"{kategorie}: {gesamtflaeche/10000:.2f} Hektar")
+    
