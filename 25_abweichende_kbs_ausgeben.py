998483e577b6b4ce8e61dc5b5f2eba48148f669b
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2025-09-22 22:43:36 +0200

    kbs und legende

---
+++ 25_abweichende_kbs_ausgeben.py
@@ -0,0 +1,49 @@
+"""
+
+Dieses Mini-Tool baut auf der Metadaten-Ausgabe auf und gibt mir das KBS aller 
+eingeladenen Layer in einem HTML aus, diesmal aber nur alle Layer, die nicht 
+das Projekt-KBS haben. Diese sind potentiell anfälliger für Fehler. Außerdem wird 
+geprüft, ob das KBS überhaupt gültig ist.
+
+"""
+
+from qgis.core import QgsProject
+from qgis.utils import iface
+import webbrowser # standardbrowser wird zum öffnen benutzt
+import os # für os.path.abspath("layer_metadaten.html")
+
+
+layers = QgsProject.instance().mapLayers().values()
+project_crs = QgsProject.instance().crs()
+
+# hier kann normaler html code verwendet werden
+html = ["<h3>Layer mit abweichendem KBS</h3>", "<table border='1' cellpadding='3'>"]
+html.append("<tr><th>Name</th><th>Abweichendes KBS</th>")
+
+for layer in layers: # alle layer in einer schleife durchgehen und metadaten rausziehen
+    
+    # Haben überhaupt alle Layer ein gültiges KBS? 
+    if layer.crs().isValid() is False:
+       print(f"KBS von Layer {layer} nicht gültig!") 
+        
+    # Entspricht das Layer-KBS nicht dem Projekt-KBS? 
+    if not layer.crs() == project_crs:
+        
+        name = layer.name()
+        
+        crs = layer.crs().authid() if layer.crs().isValid() else "—"
+        
+        
+        html.append(f"<tr><td>{name}</td><td>{crs}</td>")
+
+html.append("</table>")
+
+# direkte ausgabe im qgis interface mit pushMessage und pushInfo
+iface.messageBar().pushMessage("Layer mit abweichendem KBS", "html wurde erstellt – siehe unten", level=0)
+iface.messageBar().pushInfo("Layer mit abweichendem KBS", "\n".join(html))
+
+pfad = os.path.abspath("abweichendes_kbs.html")
+with open(pfad, "w", encoding="utf-8") as ausgabe:
+    ausgabe.write("\n".join(html))
+
+webbrowser.open(f"file://{pfad}") # öffnet den angegebenen pfad
---
+++ 26_legende_zu_png.py
@@ -0,0 +1,54 @@
+"""
+
+Die Legendeneinträge aller Layer werden als PNG exportiert. Kann schnell angepasst
+werden, um nur bestimmte Legendeneinträge zu exportieren. Dient zum Verschaffen 
+eines Überblicks sowie zukünftiger Automatisierung von Legendenexpoirten mithilfe
+dieses Codeblocks. Er gibt allerdings nur den Anfang der Legende aus. Muss noch 
+verbessert werden!
+
+"""
+from qgis.core import (QgsProject, QgsLegendRenderer,
+QgsMapSettings, QgsMapRendererParallelJob)
+from qgis.PyQt.QtGui import QImage, QPainter
+from qgis.PyQt.QtCore import QSize, QRectF
+
+import datetime # für timestamps der legende
+import os 
+
+
+
+root = QgsProject.instance().layerTreeRoot()
+
+fensterbreite = 300 
+
+
+
+
+
+
+model = QgsLayerTreeModel(root)
+model.setFlag(QgsLayerTreeModel.ShowLegend, True)
+
+view = QgsLayerTreeView()
+view.setModel(model)
+
+
+view.resize(fensterbreite, view.sizeHint().height())
+
+scene = QGraphicsScene()
+scene.addWidget(view)
+
+width = view.width()
+height = view.sizeHint().height()
+image = QImage(width, height, QImage.Format_ARGB32)
+image.fill(0xFFFFFFFF)  # weiße farbe
+
+painter = QPainter(image) # hier wird der inhalt in das bild gerendert
+scene.render(painter)
+painter.end()
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+output_path = os.path.abspath(f"layer_legend_{timestamp}.png")
+image.save(output_path)
+
+print(f"Legende als png Datei gespeichert: {output_path}")
