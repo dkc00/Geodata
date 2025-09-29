814a3237c2073be924181ba998d262f376d39385
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2025-09-29 11:31:40 +0200

    Funktion flaechenstatistik_auslesen

--- skripte/python/flaechen_auslesen.py
+++ skripte/python/flaechen_auslesen.py
@@ -16,31 +16,32 @@ import numpy as np # die flächen aller seen werden mit numpy angesprochen
 shp_path = r"C:\Users\Daniel Koch\Desktop\Fernerkundung\Daten\Shapefiles\irgendwelche_lagunen.shp"
 filename = os.path.basename(shp_path[:-4])
 
-lyr = QgsVectorLayer(shp_path, filename, 'ogr')
-
-if lyr.isValid() and filename not in [l.name() for l in QgsProject.instance().mapLayers().values()]:
-    QgsProject.instance().addMapLayer(lyr)
-    iface.messageBar().pushMessage("Glückwunsch!", "Layer erfolgreich eingeladen!", Qgis.Success, 5)
-else: 
-    iface.messageBar().pushMessage("Layer bereits eingeladen.", Qgis.Info, 5)
-
+def flaechenstatistik_auslesen(shp_path, filename): 
     
-for feat in lyr.getFeatures(): 
-    att = feat.attributes()
-    name = att[2]
-    area = float(att[1])
-    print(f" Größe des Gewässers {name} in Hektar: {area/10000:.2f}")
-    # hier wurde nachträglich :.2f eingefügt, um eine auf zwei Dezimalstellen 
-    # nach dem Komma gerundete Hektarangabe zu haben. 
-features = list(lyr.getFeatures())
-
-areas = [feat['area'] for feat in lyr.getFeatures()]
-print(f"Die Durchschnittsfläche beträgt{np.mean(areas)/10000:.2f} Hektar.")
-
-min_feat = min(features, key=lambda f: f['area'])
-max_feat = max(features, key=lambda f: f['area'])
-
-print(f"Die kleinste Fläche hat der See {min_feat['Name']} mit {min_feat['area']/10000:.2f} ha")
-print(f"Die größte Fläche hat der See {max_feat['Name']} mit {max_feat['area']/10000:.2f} ha")
-
+    lyr = QgsVectorLayer(shp_path, filename, 'ogr')
     
+    if lyr.isValid() and filename not in [l.name() for l in QgsProject.instance().mapLayers().values()]:
+        QgsProject.instance().addMapLayer(lyr)
+        iface.messageBar().pushMessage("Glückwunsch!", "Layer erfolgreich eingeladen!", Qgis.Success, 5)
+    else: 
+        iface.messageBar().pushMessage("Layer bereits eingeladen.", Qgis.Info, 5)
+        
+    for feat in lyr.getFeatures(): 
+        att = feat.attributes()
+        name = att[2]
+        area = float(att[1])
+        print(f" Größe des Gewässers {name} in Hektar: {area/10000:.2f}")
+        # hier wurde nachträglich :.2f eingefügt, um eine auf zwei Dezimalstellen 
+        # nach dem Komma gerundete Hektarangabe zu haben. 
+    features = list(lyr.getFeatures())
+    
+    areas = [feat['area'] for feat in lyr.getFeatures()]
+    print(f"Die Durchschnittsfläche beträgt{np.mean(areas)/10000:.2f} Hektar.")
+    
+    min_feat = min(features, key=lambda f: f['area'])
+    max_feat = max(features, key=lambda f: f['area'])
+    
+    print(f"Die kleinste Fläche hat der See {min_feat['Name']} mit {min_feat['area']/10000:.2f} ha")
+    print(f"Die größte Fläche hat der See {max_feat['Name']} mit {max_feat['area']/10000:.2f} ha")
+
+flaechenstatistik_auslesen(shp_path, filename)    
