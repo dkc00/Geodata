590d5d14f3674040f16a9312b7d905eac52c870c
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2025-10-01 08:38:51 +0200

    Funktion layer_sichtbarkeit()

--- skripte/python/layer_sichtbarkeit_nach_massstab.py
+++ skripte/python/layer_sichtbarkeit_nach_massstab.py
@@ -11,18 +11,25 @@ QGIS-Projekt auf einen gewissen Maßstab. Wir benötigen nur QgsProject und ifac
 from qgis.core import QgsProject
 from qgis.utils import iface
 
-lyr = QgsProject.instance().mapLayersByName("TK25")[0] # hier für eine TK 1: 25.000
-print(lyr)
+lyr_name = "" # HIER LAYER-NAME ANGEBEN
+min_scale = 50000 # erst sichtbar ab maßstab 1: 50.000 
+max_scale = 0 # keine maximalgrenze
 
-# print(iface.mapCanvas().scale()) # aktuellen kartenmaßstab prüfen, nur als test
-# sollte denselben wert ergeben wie unten in der qgis bar
+def layer_sichtbarkeit(lyr_name, min_scale, max_scale): 
 
+  lyr = QgsProject.instance().mapLayersByName(lyr_name)[0] # hier für eine TK 1: 25.000
+  # print(lyr)
+  
+  # print(iface.mapCanvas().scale()) # aktuellen kartenmaßstab prüfen, nur als test
+  # sollte denselben wert ergeben wie unten in der qgis bar
+  
+  lyr.setMinimumScale(min_scale) 
+  lyr.setMaximumScale(max_scale) 
+  
+  lyr.setScaleBasedVisibility(True) 
+  # Das muss auf True stehen, damit man setMinimumScale und setMaximumScale benutzen kann
+  
+  iface.mapCanvas().refresh() # damit qgis die änderung sofort übernimmt, .mapCanvas()
 
-lyr.setMinimumScale(50000) # erst ab 1: 50.000 
-lyr.setMaximumScale(0) # keine maximalgrenze
-
-lyr.setScaleBasedVisibility(True) 
-# Das muss auf True stehen, damit man setMinimumScale und setMaximumScale benutzen kann
-
-iface.mapCanvas().refresh() # damit qgis die änderung sofort übernimmt, .mapCanvas()
+# layer_sichtbarkeit(lyr_name, min_scale, max_scale)
 
