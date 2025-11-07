44dbcbe3e74b3dd60b607dfc6f079e62c3306a94
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2025-11-07 10:18:23 +0100

    Inputs übersichtlicher schreiben

--- skripte/R/raster_groeßer_und_nullwerte_einfuegen.R
+++ skripte/R/raster_groeßer_und_nullwerte_einfuegen.R
@@ -7,16 +7,17 @@ library(terra)
 # RASTER GROESSER MACHEN (mit Wert "0" für neue Felder)
 
 rastklein <- rast("...") # pfad eingeben
-
+output_path <- "" # output_path angeben 
+crs_code <- "" # z.B. EPSG:25833
 
 ext_neu <- ext(451228, 452852, 5944303, 5945765)  # extent angeben (hier in epsg:25833)
 
 
-crs(rastklein) <- "EPSG:25833" # crs anpassen
+crs(rastklein) <- crs_code 
 rastklein_neu <- extend(rastklein, ext_neu)
 rastklein_neu[is.na(rastklein_neu[])] <- 0
 
-writeRaster(rastklein_neu, "", overwrite = TRUE) # output path anpassen
+writeRaster(rastklein_neu, output_path, overwrite = TRUE) # output path anpassen
 
 
 
