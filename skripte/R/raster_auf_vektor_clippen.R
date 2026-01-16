4e297c767be2a7275b0a7ff3102ca2319ff3393f
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2026-01-16 09:15:00 +0100

    Enhance comments for raster clipping logic
    Added comments to clarify the clipping process for the shapefile against the raster extent.

--- skripte/R/raster_auf_vektor_clippen.R
+++ skripte/R/raster_auf_vektor_clippen.R
@@ -16,6 +16,16 @@ if (!st_crs(shp) == crs(raster)) {
   shp <- st_transform(shp, crs(raster)) #crs transformieren wenn erforderlich (if abfrage)
 }
 
+# Ist die Gesamt-Vektordatei deutlich größer als das Raster, und die TIF-Datei (z.B. Flurabstandskarte) soll lediglich auf den relevanten Ausschnitt der Vektordatei (z.B. Landesgrenzen Niedersachsens) zugeschnitten werden? 
+# Dann die drei folgenden Zeilen aktivieren, diese clippen das eingeladene Shapefile auf die Intersection von Shape und Raster-Ausdehnung. 
+
+# raster_extent <- st_as_sfc(st_bbox(raster))
+ 
+# st_crs(raster_extent) <- st_crs(shp)
+ 
+# shp_clip <- st_intersection(shp, raster_extent)
+
+
 shp_vect <- vect(shp)
 
 raster_clipped <- mask(crop(raster, shp_vect), shp_vect) # die logik ist: mask(crop(raster, vektor), vektor)
