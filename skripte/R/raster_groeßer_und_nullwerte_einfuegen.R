cb7589657566ebe63c929858382c62bcf6830c8c
Daniel León Etiénne Koch <146568637+dkc00@users.noreply.github.com>
2026-01-27 13:20:28 +0100

    Update
    Updated raster path and output path variables for clarity.

--- skripte/R/raster_groeßer_und_nullwerte_einfuegen.R
+++ skripte/R/raster_groeßer_und_nullwerte_einfuegen.R
@@ -3,8 +3,9 @@ library(terra)
 
 # RASTER GROESSER MACHEN (mit Wert "0" für neue Felder)
 
-rastklein <- rast("...") # pfad eingeben
-output_path <- "" # output_path angeben 
+rast_path <- "" # raster-pfad eingeben
+rastklein <- rast(rast_path) # pfad eingeben
+output_path <- sub("\\.tif$", "gross.tif", rast_path)
 crs_code <- "" # z.B. EPSG:25833
 
 ext_neu <- ext(451228, 452852, 5944303, 5945765)  # extent angeben (hier in epsg:25833)
